0226385274e9f881dd032e713aea05b1b224858b
kurr3i <joshuandrade777@gmail.com>
2025-11-21 10:45:21 -0600

    Fixed huskylens_loop error

--- Raspberry/main.py
+++ Raspberry/main.py
@@ -7,6 +7,7 @@ from Core.event_bus import event_publisher
 from Core.serial_communication import serial_listen_loop
 from Core.events import SerialDataReceivedEvent, HuskyLensObjectDetectedEvent
 import Core.handlers
+import Core.huskylens_reader
 in_sis = False
 data = None
 
