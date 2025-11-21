f63a9861b5dfff20e601a36f6144437a29ba2f38
kurr3i <joshuandrade777@gmail.com>
2025-11-20 22:18:44 -0600

    Third huskylens test

--- Raspberry/huskylenstest.py
+++ Raspberry/huskylenstest.py
@@ -1,8 +1,12 @@
-import serial
+import serial, time
 
-hl = serial.Serial("/dev/serial0", 9600, timeout=1)
+hl = serial.Serial("/dev/serial0", 9600, timeout=0.2)
+
+cmd = bytes([0x55, 0xAA, 0x20, 0x00, 0x00, 0x00, 0x00, 0x20])
 
 while True:
-    b = hl.read(1)
-    if b:
-        print("Byte:", b)
+    hl.write(cmd)
+    time.sleep(0.1)
+    data = hl.read(200)
+    if data:
+        print("RAW:", data.hex())
