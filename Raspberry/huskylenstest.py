a3c60100829f82aeb94e0060b8232a7118b31976
kurr3i <joshuandrade777@gmail.com>
2025-11-20 22:14:43 -0600

    Second huskylens test

--- Raspberry/huskylenstest.py
+++ Raspberry/huskylenstest.py
@@ -1,13 +1,8 @@
-import serial, time
+import serial
 
-hl = serial.Serial("/dev/serial0", 9600, timeout=0.1)
-
-# pedir datos (comando del protocolo)
-cmd = bytes([0x55, 0xAA, 0x11, 0x00, 0x00, 0x00, 0x00, 0x22])
+hl = serial.Serial("/dev/serial0", 9600, timeout=1)
 
 while True:
-    hl.write(cmd)         # pedir objetos
-    time.sleep(0.1)
-    data = hl.read(hl.in_waiting)
-    if data:
-        print(data)       # respuesta cruda de HuskyLens
+    b = hl.read(1)
+    if b:
+        print("Byte:", b)
